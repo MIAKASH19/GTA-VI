23a22820d80493fa78e6699dcb24efca27549a80
MI AKASH <mszharul1990@gmail.com>
2025-05-17 00:26:30 +0600

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import WorkButton from "./Components/WorkButton";
+import RippleButton from "./Components/RippleButton";
 
 const App = () => {
   return (
-    <div className='bg-zinc-900 text-white w-full h-screen'>Hi Akash</div>
-  )
-}
+    <div className="bg-zinc-900 text-white flex items-start justify-start w-full h-screen p-20 gap-20">
+      <WorkButton />
+      <RippleButton>Get Started</RippleButton>
+    </div>
+  );
+};
 
-export default App
+export default App;
